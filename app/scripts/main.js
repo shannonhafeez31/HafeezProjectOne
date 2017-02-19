e312d50b50f660a68560f2f0257f12cedb04b6e1
Shannon Hafeez <shannonhafeez3111@knights.ucf.edu>
2017-02-18 22:26:19 -0500

    build prototype

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+   $('ul.tabs').tabs('select_tab', 'tab_id');
+
+  $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+  $('#showAbout').click(function() {
+    $('html,body').animate({
+        scrollTop: $('.aboutme').offset().top},
+        'slow');
+});
+
+$('#showSkills').click(function() {
+  $('html,body').animate({
+      scrollTop: $('.skils').offset().top},
+      'slow');
+});
+
+$('#showPortfolio').click(function() {
+  $('html,body').animate({
+      scrollTop: $('.portfolio').offset().bottom},
+      'slow');
+});
+
+
+});
