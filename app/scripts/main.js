687b6d2dfc7d2b26fa784893c8d8dfb2f0953bfa
Shannon Hafeez <shannonhafeez3111@knights.ucf.edu>
2017-03-04 23:22:42 -0500

    Edited D3 chart

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -27,98 +27,274 @@ $(document).ready(function(){
 
 		 $('#intro').addClass('animated infinite bounce');
 
-		 $( 'a.skills' ).click(function() {
+
+$('.chart3').hide();
+
+		 $( '#webBtn' ).click(function() {
+        $('.chart_two').fadeOut();
+         $('.chart_three').fadeOut();
+        	 chart();
+             $('.chart').fadeIn();
 			  	$('.chart').addClass('animated bounceInDown');
-					 chart();
-});
+
+      });
+
+      $( '#graphicBtn' ).click(function() {
+        $('.chart').fadeOut();
+         $('.chart_three').fadeOut();
+         chartTwo();
+          $('.chart_two').fadeIn();
+ 			  	$('.chart_two').addClass('animated bounceInDown');
+
+       });
+
+       $( '#marketingBtn' ).click(function() {
+         $('.chart').fadeOut();
+         $('.chart_two').fadeOut();
+          chartThree();
+           $('.chart_three').fadeIn();
+  			  	$('.chart_three').addClass('animated bounceInDown');
+
+        });
+
 
 
 
 
 
  //d3 code
+ function chart(){
+var dataset = {
+     'children': [{
+         'facilityId': 'Microsoft Office',
+         'responseCount': 2
+     }, {
+         'facilityId': 'Adobe',
+         'responseCount': 2
+     }, {
+         'facilityId': 'PHP',
+         'responseCount': 1
+     }, {
+         'facilityId': 'CSS',
+         'responseCount': 2
+     }, {
+         'facilityId': 'HTML',
+         'responseCount': 3
+     }, {
+         'facilityId': 'JavaScript',
+         'responseCount': 1
+     }]
+ };
 
+ var diameter = 600;
+ var color = d3.scaleOrdinal(d3.schemeCategory20);
 
+ var bubble = d3.pack(dataset)
+         .size([diameter, diameter])
+         .padding(1.5);
+ var svg = d3.select('.chart')
+         .append('svg')
+         .attr('width', diameter)
+         .attr('height', diameter)
+         .attr('class', 'bubble');
 
+ var nodes = d3.hierarchy(dataset)
+         .sum(function(d) { return d.responseCount; });
 
+ var node = svg.selectAll('.node')
+         .data(bubble(nodes).descendants())
+         .enter()
+         .filter(function(d){
+             return  !d.children
+         })
+         .append('g')
+         .attr('class', 'node')
+         .attr('transform', function(d) {
+             return 'translate(' + d.x + ',' + d.y + ')';
+         });
 
-//d3 code
+ node.append('title')
+         .text(function(d) {
+             return d.facilityId;
+         });
 
-function chart(){
-	var dataset = {
-        'children': [{
-            'facilityId': 'Microsoft Office',
-            'responseCount': 2
-        }, {
-            'facilityId': 'Adobe',
-            'responseCount': 2
-        }, {
-            'facilityId': 'PHP',
-            'responseCount': 1
-        }, {
-            'facilityId': 'CSS',
-            'responseCount': 2
-        }, {
-            'facilityId': 'HTML',
-            'responseCount': 3
-        }, {
-            'facilityId': 'JavaScript',
-            'responseCount': 1
-        }]
-    };
+ node.append('circle')
+         .attr('r', function(d) {
+             return d.r;
+         })
+         .style('fill', function(d) {
+             return color(d.facilityId);
+         });
 
-    var diameter = 600;
-    var color = d3.scaleOrdinal(d3.schemeCategory20);
+ node.append('text')
+         .attr('dy', '.3em')
+         .style('text-anchor', 'middle')
+         .text(function(d) {
+             return d.data.facilityId.substring(0, d.r / 3);
+         });
 
-    var bubble = d3.pack(dataset)
-            .size([diameter, diameter])
-            .padding(1.5);
-    var svg = d3.select('.chart')
-            .append('svg')
-            .attr('width', diameter)
-            .attr('height', diameter)
-            .attr('class', 'bubble');
-
-    var nodes = d3.hierarchy(dataset)
-            .sum(function(d) { return d.responseCount; });
-
-    var node = svg.selectAll('.node')
-            .data(bubble(nodes).descendants())
-            .enter()
-            .filter(function(d){
-                return  !d.children
-            })
-            .append('g')
-            .attr('class', 'node')
-            .attr('transform', function(d) {
-                return 'translate(' + d.x + ',' + d.y + ')';
-            });
-
-    node.append('title')
-            .text(function(d) {
-                return d.facilityId;
-            });
-
-    node.append('circle')
-            .attr('r', function(d) {
-                return d.r;
-            })
-            .style('fill', function(d) {
-                return color(d.facilityId);
-            });
-
-    node.append('text')
-            .attr('dy', '.3em')
-            .style('text-anchor', 'middle')
-            .text(function(d) {
-                return d.data.facilityId.substring(0, d.r / 3);
-            });
-
-    d3.select(self.frameElement)
-            .style('height', diameter + 'px');
+ d3.select(self.frameElement)
+         .style('height', diameter + 'px');
 
 
 }
+
+function chartTwo(){
+var dataset = {
+    'children': [{
+        'facilityId': 'Adobe Photoshop',
+        'responseCount': 2
+    }, {
+        'facilityId': 'Adobe Dreamweaver',
+        'responseCount': 2
+    }, {
+        'facilityId': 'Adobe Indesign',
+        'responseCount': 1
+    }, {
+        'facilityId': 'Adobe Premire',
+        'responseCount': 2
+    }, {
+        'facilityId': 'Adobe Illustrator',
+        'responseCount': 3
+    }, {
+        'facilityId': 'AutoDesk Maya',
+        'responseCount': 1
+    }]
+};
+
+var diameter = 600;
+var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+var bubble = d3.pack(dataset)
+        .size([diameter, diameter])
+        .padding(1.5);
+var svg = d3.select('.chart_two')
+        .append('svg')
+        .attr('width', diameter)
+        .attr('height', diameter)
+        .attr('class', 'bubble');
+
+var nodes = d3.hierarchy(dataset)
+        .sum(function(d) { return d.responseCount; });
+
+var node = svg.selectAll('.node')
+        .data(bubble(nodes).descendants())
+        .enter()
+        .filter(function(d){
+            return  !d.children
+        })
+        .append('g')
+        .attr('class', 'node')
+        .attr('transform', function(d) {
+            return 'translate(' + d.x + ',' + d.y + ')';
+        });
+
+node.append('title')
+        .text(function(d) {
+            return d.facilityId;
+        });
+
+node.append('circle')
+        .attr('r', function(d) {
+            return d.r;
+        })
+        .style('fill', function(d) {
+            return color(d.facilityId);
+        });
+
+node.append('text')
+        .attr('dy', '.3em')
+        .style('text-anchor', 'middle')
+        .text(function(d) {
+            return d.data.facilityId.substring(0, d.r / 3);
+        });
+
+d3.select(self.frameElement)
+        .style('height', diameter + 'px');
+
+
+}
+
+
+function chartThree(){
+var dataset = {
+   'children': [{
+       'facilityId': 'Microsoft PPT',
+       'responseCount': 2
+   }, {
+       'facilityId': 'Microsoft Excel',
+       'responseCount': 2
+   }, {
+       'facilityId': 'Branding',
+       'responseCount': 1
+   }, {
+       'facilityId': 'Social Media',
+       'responseCount': 2
+   }, {
+       'facilityId': 'Microsoft Word',
+       'responseCount': 3
+   }, {
+       'facilityId': 'Microsoft Access',
+       'responseCount': 1
+   }]
+};
+
+var diameter = 600;
+var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+var bubble = d3.pack(dataset)
+       .size([diameter, diameter])
+       .padding(1.5);
+var svg = d3.select('.chart_three')
+       .append('svg')
+       .attr('width', diameter)
+       .attr('height', diameter)
+       .attr('class', 'bubble');
+
+var nodes = d3.hierarchy(dataset)
+       .sum(function(d) { return d.responseCount; });
+
+var node = svg.selectAll('.node')
+       .data(bubble(nodes).descendants())
+       .enter()
+       .filter(function(d){
+           return  !d.children
+       })
+       .append('g')
+       .attr('class', 'node')
+       .attr('transform', function(d) {
+           return 'translate(' + d.x + ',' + d.y + ')';
+       });
+
+node.append('title')
+       .text(function(d) {
+           return d.facilityId;
+       });
+
+node.append('circle')
+       .attr('r', function(d) {
+           return d.r;
+       })
+       .style('fill', function(d) {
+           return color(d.facilityId);
+       });
+
+node.append('text')
+       .attr('dy', '.3em')
+       .style('text-anchor', 'middle')
+       .text(function(d) {
+           return d.data.facilityId.substring(0, d.r / 3);
+       });
+
+d3.select(self.frameElement)
+       .style('height', diameter + 'px');
+
+
+}
+
+
+
+
 
 
 
